3da48b968063860425761560dec1ffa1bda180b0
Chasith-Randima <63777752+Chasith-Randima@users.noreply.github.com>
2023-07-19 08:50:29 +0530

    Time: 137 ms (73.26%), Space: 15 MB (33.65%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # nums.sort()
+        # n = len(nums)
+        # return nums[n//2]
+        count = len(nums)
+        tempDict = defaultdict(int)
+        
+        for num in nums:
+            tempDict[num] += 1
+        
+        count = count//2
+        
+        for key,value in tempDict.items():
+            if value > count:
+                return key
+        return 0
